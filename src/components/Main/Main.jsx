141f1c17895571ab9a527ef2747373596cc1fb91
Jess Sang <jessicamsang@hotmail.com>
2025-04-10 09:09:04 -0400

    start of responsive design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,3 @@
-import "./Main.css";
 import { defaultClothingItems } from "../../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
