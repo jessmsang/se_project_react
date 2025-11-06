c09889180a6358019118b5f4f5f0c90a4175a664
Jess Sang <jessicamsang@hotmail.com>
2025-11-06 12:08:41 -0500

    update url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.wtwr-js.jumpingcrab.com"
+    ? "https://api.wtwr.devbyjess.com"
     : "http://localhost:3001";
 
 export const weatherOptions = [
